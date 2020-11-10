6ce5f0f35aaf0355e954c588172c5347dc47c643
karlluo(罗星) <karlluo@futunn.com>
2020-11-10 10:57:51 +0800

    melpa using qinghua

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -15,10 +15,9 @@
 
 ;;; Standard package repositories
 
-(add-to-list 'package-archives '( "melpa" . "https://melpa.org/packages/") t)
+;; (add-to-list 'package-archives '( "melpa" . "http://melpa.org/packages/") t)
 ;; Official MELPA Mirror, in case necessary.
-;;(add-to-list 'package-archives (cons "melpa-mirror" (concat proto "://www.mirrorservice.org/sites/melpa.org/packages/")) t)
-
+(add-to-list 'package-archives '( "melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/") t)
 
 
 ;; Work-around for https://debbugs.gnu.org/cgi/bugreport.cgi?bug=34341
